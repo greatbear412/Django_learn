955fc9973cc313cbae4d02917196c333f496151d
xyj <yongjiu.xu@easystack.cn>
2020-01-22 18:09:33 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -4,8 +4,14 @@
 cd ~/code/mydjango
 source .venv/bin/activate
 
-echo "启动服务的端口号:8000"
-python3 mysite/manage.py runserver 8000;
-# while read po;
-#     do python3 mysite/manage.py runserver ${po};
-#     done
+echo "启动服务的端口号:8000，是否继续? (y/你期望的端口号)"
+# python3 mysite/manage.py runserver 8000;
+while read po;
+    do 
+        if [ ${po} == "y" ]
+        then
+            python3 mysite/manage.py runserver 8000;
+        else
+            python3 mysite/manage.py runserver ${po};
+        fi
+    done
